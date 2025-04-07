8b8712ed1931d1fe2984ff8b6fae96856c51de2b
lalky <bilaldemonk@gmail.com>
2025-04-08 01:45:35 +0700

    Initial deploy: setup Vercel serverless and public

---
+++ api/sendMail.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ path: '../.env' });
+const nodemailer = require('nodemailer')
+
+module.exports = async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status (405).json({ message: 'Method Not Allowed' });
+    }
+
+    const { nama, email, message } = req.body;
+
+    // validasi input
+    if (!nama || !email || !message) {
+        return res.status(400).send('Semua bidang harus diisi!.');
+    }
+
+    // Validasi email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'Format email salah.' });
+    }
+
+    // Konfigurasi transporter nodemailer
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_RECEIVER,
+        replyTo: email,
+        subject: `Pesan dari ${nama}`,
+        text: message
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email berhasil dikirim:', info.response);
+        res.status(200).json({ message: 'Email berhasil dikirim!' });
+    } catch (error) {
+        console.error('Gagal mengirim email:', error);
+        res.status(500).json({ message: 'Terjadi kesalahan saat mengirim email.'});
+    }
+};
---
+++ public/js/script.js
@@ -0,0 +1,101 @@
+// Ambil elemen
+const btnContact = document.getElementById('btn-contact');
+const modal = document.getElementById('modal-contact');
+const modalClose = document.getElementById('modal-close');
+const form = document.getElementById('contact-form');
+
+// Ketika tombol "hubungi developer" diklik
+btnContact.addEventListener('click', () => {
+    modal.style.display = 'flex'; // tampilkan modal
+});
+
+// Ketika tombol "close" diklik
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none'; // sembunyikan modal
+    form.reset(); // Reset form
+});
+
+// Ketika pengguna mengklik area di luar modal
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none'; // sembunyikan modal
+        form.reset(); // Reset form
+    }
+});
+
+// efek pada kursor
+document.addEventListener("mousemove", (e) => {
+    const container = document.querySelector(".not-found-container");
+    const { clientX, clientY } = e;
+
+    // Efek parallax dengan mengikuti posisi kursor
+    const moveX = (clientX / window.innerWidth) * 10 - 5;
+    const moveY = (clientY / window.innerHeight) * 10 - 5;
+
+    container.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+// FETCH API
+async function dataSend() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const url = 'api/sendMail';
+    const data = { nama: name, email: email, message: message };
+
+    console.log('Mengirim data:', data);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        console.log('Status respons:', response.status);
+
+        // menangani respon yang diterima
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log('Data respons:', responseData);
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: 'Pesan Anda berhasil dikirim.',
+            });
+
+            form.reset(); // reset form setelah sukses
+            modal.style.display = 'none'; // tutup modal
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: result.message || 'Terjadi kesalahan saat mengirim pesan.',
+            });
+            console.error('Server error!');
+        }
+    } catch (error) {
+        console.error('Error saat mengirim email:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Terjadi kesalahan! Periksa koneksi internet Anda.',
+        });
+    }
+}
+
+// eventlistener untuk submit form
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // mencegah form reload halaman
+    dataSend();
+});
+
+document.getElementById('btn-home').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: 'Info',
+        text: 'Maaf, fitur ini belum tersedia saat ini.',
+        icon: 'info'
+    });
+});
