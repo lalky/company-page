7104d9a9fd67562a9ec1de88f77c8bd092da3815
mas_lalkyy <114236956+lalky@users.noreply.github.com>
2025-04-08 18:24:11 +0700

    Delete api directory

--- api/sendMail.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('POST request received!');
-    return res.status(200).json({ message: 'POST berhasil!' });
-  } else {
-    return res.status(405).json({ message: 'Metode tidak diizinkan' });
-  }
-}
