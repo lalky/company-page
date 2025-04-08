7a111d380499a02460491fd076b8be7be958de16
mas_lalkyy <114236956+lalky@users.noreply.github.com>
2025-04-08 17:37:15 +0700

    Update sendMail.js

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,13 +1,13 @@
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        
-        const { nama, email, message } = req.body;
-        
-        if (!nama || !email || !message) {
-            return res.status(400).send('Semua bidang harus diisi!.');
-        }
-            return res.status(200).json({ message: `Halo ${name}, pesan kamu: "${message}" sudah diterima yaa.` });
-    }
-    
-    res.status(200).json({ message: 'Success from Vercel API!' });
-};
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  const { nama, email, message } = req.body;
+
+  if (!nama || !email || !message) {
+    return res.status(400).json({ message: 'Semua bidang harus diisi.' });
+  }
+
+  return res.status(200).json({ message: 'Email berhasil dikirim!' });
+}
